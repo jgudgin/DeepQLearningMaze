1f08d4530db87d3bb1c17162757f82a391be53df
jgudgin <j.gudgin146@gmail.com>
2026-08-24 12:25:55 +1200

    replace Action.java with enum
    Action used deltaX/deltaY with NORTH as (0, -1), treating x as the
    column. State.getSurroundings treated x as the row, checking north as maze[x-1][y] which caused the agent would check north for
    a wall and then move west.
    
    Converting Action to an enum allows using ordinal() instead of the previous switch and COUNT to size the networks output layer

--- src/neuralnetwork/Action.java
+++ src/neuralnetwork/Action.java
@@ -1,67 +1,34 @@
 package neuralnetwork;
 
 //action class holding information about the types of actions available for the game
-public class Action {
+public enum Action {
+    NORTH(-1, 0),
+    SOUTH(1,0),
+    EAST(0,1),
+    WEST(0,-1);
 
-    public static final Action NORTH = new Action("NORTH", 0, -1);
-    public static final Action SOUTH = new Action("SOUTH", 0, 1);
-    public static final Action EAST = new Action("EAST", 1, 0);
-    public static final Action WEST = new Action("WEST", -1, 0);
+    //how many actions there are / size of the network output layer
+    public static final int COUNT = values().length;
 
-    private final String name;
-    private final int deltaX;
-    private final int deltaY;
+    private final int deltaRow;
+    private final int deltaCol;
 
-    private Action(String name, int deltaX, int deltaY) {
-        this.name = name;
-        this.deltaX = deltaX;
-        this.deltaY = deltaY;
+    Action(int deltaRow, int deltaCol){
+        this.deltaRow = deltaRow;
+        this.deltaCol = deltaCol;
     }
 
-    public int getDeltaX() {
-        return deltaX;
+    public int getDeltaRow() {
+        return deltaRow;
     }
 
-    public int getDeltaY() {
-        return deltaY;
+    public int getDeltaCol() {
+        return deltaCol;
     }
 
-    @Override
-    public String toString() {
-        return name;
-    }
-
-    //method for neural network input
-    public double[] convertToInput() {
-        switch (this.name) {
-            case "NORTH":
-                return new double[]{1, 0, 0, 0};
-            case "SOUTH":
-                return new double[]{0, 1, 0, 0};
-            case "EAST":
-                return new double[]{0, 0, 1, 0};
-            case "WEST":
-                return new double[]{0, 0, 0, 1};
-            default:
-                throw new IllegalArgumentException("Unknown action: " + this.name);
-        }
-
-    }
-    
-    //method for direct indexing (for updating Q-values)
+    //the position of this action in the networks output array
+    //ensures that the moves are correctly mapped to what the agent choses
     public int index() {
-        switch (this.name) {
-            case "NORTH":
-                return 0;
-            case "SOUTH":
-                return 1;
-            case "EAST":
-                return 2;
-            case "WEST":
-                return 3;
-            default:
-                throw new IllegalArgumentException("Unknown action: " + this.name);
-        }
+        return ordinal();
     }
-
 }
